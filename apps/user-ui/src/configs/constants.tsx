ede10055ddeca8188d61158c218f6b70f9a4ccac
edris-poursadeghi <edris.emperator@gmail.com>
2025-09-18 02:02:30 +0330

    main add header sticky

---
+++ apps/user-ui/src/configs/constants.tsx
@@ -0,0 +1,9 @@
+import { NavItemTypes } from './global';
+
+export const navItems: NavItemTypes[] = [
+  { title: 'Home', href: '/' },
+  { title: 'Products', href: '/products' },
+  { title: 'Shops', href: '/shops' },
+  { title: 'Offers', href: '/offers' },
+  { title: 'Become A Seller', href: '/become-seller' },
+];
